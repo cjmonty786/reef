b7e62e69551e20795f56d8e0048eb66e3538d92d
Monty <cjmonty@gmail.com>
2017-11-08 23:32:58 -0500

    update angular

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { ReefPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('reef App', function() {
-  let page: ReefPage;
+describe('monty App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new ReefPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
