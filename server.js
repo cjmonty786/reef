c1043bbdf22306a022be9499792a44408e695494
Monty <cjmonty@gmail.com>
2017-11-08 23:19:22 -0500

    server file move

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-// app.set('views', __dirname + '/views');
-// app.set('view engine', 'ejs');
-
-// app.get('/', function(request, response) {
-//   response.render('pages/index');
-// });
+app.use(express.static(path.join(__dirname,'dist')));
 
 
 app.get('/',function(req,res){
+	console.log('hi')
+	console.log(__dirname);
+	res.sendFile(path.join(__dirname,'dist/index.html'))
+});
+
+app.get('/api',function(req,res){
 	res.send('hi');
 });
 
