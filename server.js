9731744940f0bbc9941bd76f750e6465ee9e9291
Monty <cjmonty@gmail.com>
2017-11-21 22:57:07 -0500

    progress

--- server.js
+++ server.js
@@ -2,22 +2,13 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var bodyParser = require('body-parser');
+var Database = require('better-sqlite3');
+var ds18b20 = require('ds18b20');
 
-
-const {
-    Client
-} = require('pg');
-
-
-
-function createClient() {
-    return new Client({
-        connectionString: process.env.DATABASE_URL || "postgres://fphjutmpurtjyn:db113a93b26d1dfb5db60a8ac723633ca1c6a5acd8f0bab88b0c6fb44d706032@ec2-54-204-41-80.compute-1.amazonaws.com:5432/dchcrnileqjk45",
-        ssl: true,
-    })
+var options = {
+    fileMustExist: true
 };
-
-
+var db = new Database('reef.db', options);
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -31,59 +22,94 @@ app.get('/', function(req, res) {
 });
 
 app.get('/api/temp', function(req, res) {
-    var client = createClient();
-    client.connect();
-    client.query('SELECT * from TEMPERATURE;', (err, result) => {
-        if (err) {
-            res.sendStatus(500)
-            client.end();
-            throw err;
-        }
-        var response = [];
-        for (let row of result.rows) {
-            response.push(row);
-        }
-        res.setHeader('Content-Type', 'application/json');
-        res.send(response);
-        client.end();
-    });
+    var rows = db.prepare('SELECT * FROM TEMPERATURE').all();
+    res.send(rows);
 });
-app.post('/api/temp', function(req, res) {
-    if (req.body && req.body.temp) {
-        var client = createClient();
-        client.connect();
-        client.query('INSERT INTO TEMPERATURE(temp) VALUES($1);', [req.body.temp], (err, result) => {
-            if (err) {
-                res.sendStatus(500)
-                client.end();
-                throw err;
-            }
-            var response = [];
-            for (let row of result.rows) {
-                response.push(row);
-            }
-            res.setHeader('Content-Type', 'application/json');
-            res.send(response);
-            client.end();
-        });
+// app.post('/api/temp', function(req, res) {
+//     if (req.body && req.body.temp) {
+//         var stmt = db.prepare('INSERT INTO TEMPERATURE(temp) VALUES(?);');
+//         var x = stmt.run(req.body.temp);
+//         if (x.changes == 1) {
+//             res.sendStatus(200);
+//         } else {
+//             res.sendStatus(500);
+//         }
+//     } else {
+//         res.sendStatus(500);
+//     }
+
+
+// });
+
+
+app.get('/api/status', function(req, res) {
+    var elapsed = Date.now() - start;
+    var remaining;
+    if (aotStatus == "off") {
+        remaining = (cycle - timeOn - elapsed) / 1000; // divide by 1000 for seconds
     } else {
-        res.sendStatus(500);
+        remaining = (timeOn - elapsed) / 1000;
     }
-
-
+    var json = {
+        aot: {
+            status: aotStatus,
+            remaining: remaining
+        }
+    };
+    res.send(json);
 });
-
-
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
 
 
+const cycle = 10000;
+const timeOn = 2000;
+var aotStatus = "off";
+var start = Date.now(); // milliseconds
+var aotTimeout = setInterval(function() {
+    start = Date.now();
+    console.log("on");
+    aotStatus = "on";
+    setTimeout(function() {
+        start = Date.now();
+        console.log("off")
+        aotStatus = "off";
+    }, timeOn)
+}, cycle);
+
+setInterval(function() {
+    var temp = ds18b20.temperatureSync('28-0316c308b5ff') * 9/5 + 32;
+    console.log(temp);
+    var stmt = db.prepare('INSERT INTO TEMPERATURE(temp) VALUES(?);');
+    stmt.run(temp);
+},5000);
 
 
 
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'cjmonty@gmail.com',
+    pass: '072586MONty'
+  }
+});
 
+var mailOptions = {
+  from: 'cjmonty@gmail.com',
+  to: '9145238363@text.republicwireless.com',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
 
 
 
@@ -102,20 +128,3 @@ app.listen(app.get('port'), function() {
 //         },60 * 1000)
 //     }, 3600 * 1000);
 // }, timeoutMs)
-
-
-var Database = require('better-sqlite3');
-var options ={
-        fileMustExist : true
-};
-var db = new Database('reef.db', options);
-
-var row = db.prepare('SELECT * FROM TEMPERATURE').all();
-console.log(row);
-
-
-
-
-
-
-
