c4967b8d73321cc39f0df989d632de8a7f5da463
Monty <cjmonty@gmail.com>
2017-12-04 14:37:16 -0500

    update angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,46 +7,36 @@ import { Http } from '@angular/http';
 })
 export class AppComponent implements OnInit {
   title = 'app';
-  lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'}
-  ];
-   // lineChart
+  lineChartData:Array<any>;
+  lineChartLabels:Array<any>;
+  lineChartOptions:any = {
+    responsive: true
+  };
+
+  lineChartLegend:boolean = true;
+  lineChartType:string = 'line';
 
    constructor(private http:Http){
 
    }
    ngOnInit(){
-      this.http.get("http://localhost:5000/api/temp").subscribe(x => {
+      this.http.get("http://192.168.0.42:5000/api/temp").subscribe(x => {
         let z:Array<any> = [];
         let data = x.json();
         let series = [];
+        let labels = [];
         for(let d of data){
           series.push(d.temp);
+          let date = new Date(d.time);
+
+          labels.push((new Date(d.time).toLocaleString("en-US",{month: '2-digit', day: '2-digit', year: '2-digit', hour: '2-digit', minute:'2-digit'})).replace(",",""));
         }
         z.push({data: series, label: 'Temp'});
         this.lineChartData = z;
+        this.lineChartLabels = labels;
     });
    }
-
-  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public lineChartOptions:any = {
-    responsive: true
-  };
-
-  public lineChartLegend:boolean = true;
-  public lineChartType:string = 'line';
- 
-  public randomize():void {
-    let _lineChartData:Array<any> = new Array(this.lineChartData.length);
-    for (let i = 0; i < this.lineChartData.length; i++) {
-      _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
-      for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-        _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-      }
-    }
-    this.lineChartData = _lineChartData;
-  }
- 
+  
   // events
   public chartClicked(e:any):void {
     console.log(e);
