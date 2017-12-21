5f22058bdbacad35a2e954a910e4cbeea88a9887
Monty <cjmonty@gmail.com>
2017-12-21 15:10:16 -0500

    converting atotimer to cron job

--- backend/atoTimer.js
+++ backend/atoTimer.js
@@ -2,16 +2,15 @@ var db = require('./db.js').getConnection();
 var config = require('./config.js');
 var Gpio = require('onoff').Gpio;
 var pump = new Gpio(21, 'out');
+var cron = require('node-schedule');
 var pumping = 0;
 var dbPumpingId;
 
 function startAtoMonitor() {
-console.log("hi");
-    setInterval(function() {
-pumping = 1;
-pump.writeSync(pumping);
-console.log("pump");        
-var stmt = db.prepare("INSERT INTO ATO(START_TIME) VALUES (CURRENT_TIMESTAMP)");
+    var pumpJob cron.scheduleJob('0 * * * *', function() {
+        pump.writeSync(pumping);
+        console.log("pump");
+        var stmt = db.prepare("INSERT INTO ATO(START_TIME) VALUES (CURRENT_TIMESTAMP)");
         dbPumpingId = stmt.run().lastInsertROWID;
         setTimeout(function() {
             pumping = 0;
@@ -21,8 +20,8 @@ var stmt = db.prepare("INSERT INTO ATO(START_TIME) VALUES (CURRENT_TIMESTAMP)");
                 stmt.run(dbPumpingId);
                 dbPumpingId = null;
             }
-        }, 6 * 1000);
-    }, 10 * 1000);
+        }, 60 * 1000);
+    });
 }
 
 function getAtoPumpStatus() {
