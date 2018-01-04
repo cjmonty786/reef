c06fed4943d4e61c5dd5856e89d7db36a83cdfeb
Monty <cjmonty@gmail.com>
2018-01-04 14:56:00 -0500

    manual ato

--- backend/atoTimer.js
+++ backend/atoTimer.js
@@ -4,24 +4,51 @@ var Gpio = require('onoff').Gpio;
 var pump = new Gpio(21, 'out');
 var cron = require('node-schedule');
 var pumping = 0;
+var overridePump = 0;
 var dbPumpingId;
 
 function startAtoMonitor() {
     var pumpJob = cron.scheduleJob('0 * * * *', function() {
-        pumping = 1;
-        pump.writeSync(pumping);
-        var stmt = db.prepare("INSERT INTO ATO(START_TIME) VALUES (CURRENT_TIMESTAMP)");
-        dbPumpingId = stmt.run().lastInsertROWID;
-        setTimeout(function() {
-            pumping = 0;
-            pump.writeSync(pumping);
-            if (dbPumpingId) {
-                var stmt = db.prepare("UPDATE ATO SET END_TIME = CURRENT_TIMESTAMP WHERE ID = ?");
-                stmt.run(dbPumpingId);
-                dbPumpingId = null;
-            }
-        }, 60 * 1000);
+        if (overridePump == 0) {
+            startPump();
+            setTimeout(function() {
+                if (overridePump == 0) {
+                    stopPump();
+                }
+            }, 60 * 1000);
+        }
     });
+}
+
+function manualStartPump() {
+    if (overridePump == 0) {
+        overridePump = 1;
+        startPump();
+    }
+}
+
+function manualStopPump() {
+    if (overridePump == 0) {
+        stopPump();
+        overridePump = 0;
+    }
+}
+
+function startPump() {
+    pumping = 1;
+    pump.writeSync(pumping);
+    var stmt = db.prepare("INSERT INTO ATO(START_TIME) VALUES (CURRENT_TIMESTAMP)");
+    dbPumpingId = stmt.run().lastInsertROWID;
+}
+
+function stopPump() {
+    pumping = 0;
+    pump.writeSync(pumping);
+    if (dbPumpingId) {
+        var stmt = db.prepare("UPDATE ATO SET END_TIME = CURRENT_TIMESTAMP WHERE ID = ?");
+        stmt.run(dbPumpingId);
+        dbPumpingId = null;
+    }
 }
 
 function getAtoPumpStatus() {
@@ -29,5 +56,7 @@ function getAtoPumpStatus() {
 }
 module.exports = {
     startAtoMonitor,
-    getAtoPumpStatus
+    getAtoPumpStatus,
+    manualStartPump,
+    manualStopPump
 };
