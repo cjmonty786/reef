2dc73bb30a052b43aa0e211ddd0a8d3f77f9896f
Monty <cjmonty@gmail.com>
2018-01-21 19:46:13 -0500

    code clean up

--- backend/atoTimer.js
+++ backend/atoTimer.js
@@ -7,6 +7,7 @@ var overridePump = 0;
 var dbPumpingId;
 const tpLink = require('tplink-smarthome-api');
 const tpClient = new tpLink.Client();
+
 function startAtoMonitor() {
     var pumpJob = cron.scheduleJob('0 * * * *', function() {
         if (overridePump == 0) {
@@ -35,23 +36,28 @@ function manualStopPump() {
 }
 
 function startPump() {
-  
-    tpClient.getDevice({host: '192.168.0.39'}).then((device) => {
-	device.setPowerState(true);
-	pumping = 1;	
-    var stmt = db.prepare("INSERT INTO ATO(START_TIME) VALUES (CURRENT_TIMESTAMP)");
-    dbPumpingId = stmt.run().lastInsertROWID;
-});
+    tpClient.getDevice({
+        host: '192.168.0.39'
+    }).then((device) => {
+        device.setPowerState(true);
+        pumping = 1;
+        var stmt = db.prepare("INSERT INTO ATO(START_TIME) VALUES (CURRENT_TIMESTAMP)");
+        dbPumpingId = stmt.run().lastInsertROWID;
+    });
 }
 
 function stopPump() {
-    pumping = 0;
-    pump.writeSync(pumping);
-    if (dbPumpingId) {
-        var stmt = db.prepare("UPDATE ATO SET END_TIME = CURRENT_TIMESTAMP WHERE ID = ?");
-        stmt.run(dbPumpingId);
-        dbPumpingId = null;
-    }
+    tpClient.getDevice({
+        host: '192.168.0.39'
+    }).then((device) => {
+        device.setPowerState(false);
+        pumping = 0;
+        if (dbPumpingId) {
+            var stmt = db.prepare("UPDATE ATO SET END_TIME = CURRENT_TIMESTAMP WHERE ID = ?");
+            stmt.run(dbPumpingId);
+            dbPumpingId = null;
+        }
+    });
 }
 
 function getAtoPumpStatus() {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Http } from '@angular/http'; 
+import { Http } from '@angular/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,42 +7,47 @@ import { Http } from '@angular/http';
 })
 export class AppComponent implements OnInit {
   title = 'app';
-  lineChartData:Array<any>;
-  lineChartLabels:Array<any>;
-  lineChartOptions:any = {
+  lineChartData: Array<any>;
+  lineChartLabels: Array<any>;
+  lineChartOptions: any = {
     responsive: true
   };
 
-  lineChartLegend:boolean = true;
-  lineChartType:string = 'line';
-
-   constructor(private http:Http){
+  lineChartLegend = true;
+  lineChartType = 'line';
+private url = 'http://192.168.0.14:5000';
+   constructor(private http: Http) {
 
    }
-   ngOnInit(){
-      this.http.get("http://192.168.0.42:5000/api/temp").subscribe(x => {
-        let z:Array<any> = [];
-        let data = x.json();
-        let series = [];
-        let labels = [];
-        for(let d of data){
+   ngOnInit() {
+      this.http.get(this.url + '/api/temp').subscribe(x => {
+        const z: Array<any> = [];
+        const data = x.json();
+        const series = [];
+        const labels = [];
+        for (const d of data){
           series.push(d.temp);
-          let date = new Date(d.time);
-
-          labels.push((new Date(d.time).toLocaleString("en-US",{month: '2-digit', day: '2-digit', year: '2-digit', hour: '2-digit', minute:'2-digit'})).replace(",",""));
+          const date = new Date(d.time + 'Z+0');
+          labels.push(
+            (date.toLocaleString('en-US',
+            {month: '2-digit', day: '2-digit', year: '2-digit', hour: '2-digit', minute: '2-digit'})).replace(',', ''));
         }
         z.push({data: series, label: 'Temp'});
         this.lineChartData = z;
         this.lineChartLabels = labels;
     });
-   }
-  
+  }
   // events
-  public chartClicked(e:any):void {
+  public chartClicked(e: any): void {
     console.log(e);
   }
- 
-  public chartHovered(e:any):void {
+  public chartHovered(e: any): void {
     console.log(e);
+  }
+  public pumpOn() {
+    this.http.post(this.url + '/api/ato', {'pump': 1}).subscribe();
+  }
+  public pumpOff() {
+    this.http.post(this.url + '/api/ato', {'pump': 0}).subscribe();
   }
 }
